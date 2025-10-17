77c58269ee9521cc50a580a2c306f46e985aa33d
Angel Eduardo Pedraza Ordoñez <leedgne@gmail.com>
2025-10-17 16:51:07 -0400

    Primer cambio.

--- src/ComprobaciónMod4.java
+++ src/ComprobaciónMod4.java
@@ -16,5 +16,10 @@ public class ComprobaciónMod4 {
     // 6: Usando "return".
     // 7: El mismo nombre que la clase que pertenece.
     // 8: El operador "new" asigna memoria a un objeto y lo inicializa empleando el constructor del objeto.
-    // 9:
+    /* 9: Se encarga de eliminar objetos a los cuales ya no son necesario en el programa, con la adición de la necesidad de espacio.
+    El método finalize() le dice al compilador que hacer antes de eliminar el objeto en el que está.Cómo debe comportarse.
+     */
+    // 10: Es la referencia que se invoca cuando se llama a un método del objeto.
+    // 11: Sí. Los puede tener.
+    // 12: void.
 }
