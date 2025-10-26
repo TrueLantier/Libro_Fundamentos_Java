c44917de002812367b93407ad1ba8f8a12cb937a
Angel Eduardo Pedraza Ordoñez <leedgne@gmail.com>
2025-10-26 10:13:23 -0400

    Ahora pruebo la línea de comandos.

--- src/ParaEjemplos.java
+++ src/ParaEjemplos.java
@@ -1,9 +1,24 @@
 public class ParaEjemplos {
     public static void main(String[] args) {
-        System.out.println( "Hay " + args.length + " argumentos de línea de comandos." );
-        System.out.println("Son");
-        for ( int i = 0; i< args.length; i++) {
-            System.out.println( "arg[" + i + "]: " + args[i] );
+        String tels[][] = {
+                { "Juan", "5555-3322"},
+                { "Mary", "5555-8976"},
+                { "Jaime", "5555-1037"},
+                { "Raquel", "5555-1400"},
+        };
+        int i;
+        if (args.length != 1) {
+            System.out.println("Uso: java Teléfono <nombre>");
+        }   else {
+            for ( i=0; i< tels.length; i++){
+                if ( tels[i][0].equals(args[0]) ) {
+                    System.out.println( tels[i][0] + ": " + tels[i][1] );
+                    break;
+                }
+            }
+            if ( i == tels.length ) {
+                System.out.println("El nombre no se encuentra.");
+            }
         }
     }
 }
