cdf795c6dce150446f256a2403968cab058e4958
Angel Eduardo Pedraza Ordoñez <leedgne@gmail.com>
2025-10-28 21:57:14 -0400

    Haciendo la comprobación del módulo 5.

---
+++ src/ComprobaciónMod5.java
@@ -0,0 +1,9 @@
+public class ComprobaciónMod5 {
+    public static void main(String[] args) {
+        double ejercicioUno[] = new double[12];
+
+        int ejercicioDos[] = {1, 2, 3, 4, 5};
+
+        double ejercicioTres[] = new double[10];
+    }
+}
--- src/ParaEjemplos.java
+++ src/ParaEjemplos.java
@@ -1,24 +1,12 @@
 public class ParaEjemplos {
     public static void main(String[] args) {
-        int val = 1;
+        int result;
 
-        for ( int i = 0; i<8; i++) {
-            for ( int t = 128; t>0; t = t/2) {
-                if ( (val & t) != 0 ) System.out.print("1 ");
-                else System.out.print("0 ");
+        for ( int i = -2; i<3; i++) {
+            result = (i != 0) ? (100/i) : 0;
+            if (i !=0) {
+                System.out.println("100 / " + i + "es " + result);
             }
-            System.out.println();
-            val <<= 1;
-        }
-        System.out.println();
-        val = 128;
-        for ( int i = 0; i<8; i++) {
-            for ( int t = 128; t>0; t=t/2) {
-                if ( (val & t) !=0 ) System.out.print("1 ");
-                else System.out.print("0 ");
-            }
-            System.out.println();
-            val >>=1;
         }
     }
 }
