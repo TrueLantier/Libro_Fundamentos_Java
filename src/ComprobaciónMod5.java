0a40dd7b024760b5303d2559f6d4bf378ae4b823
Angel Eduardo Pedraza Ordoñez <leedgne@gmail.com>
2025-10-29 21:02:30 -0400

    Haciendo la comprobación del módulo 5.

--- src/ComprobaciónMod5.java
+++ src/ComprobaciónMod5.java
@@ -1,9 +1,33 @@
 public class ComprobaciónMod5 {
     public static void main(String[] args) {
-        double ejercicioUno[] = new double[12];
+        double ejercicioUno[] = new double[10];
+        double[] ejrcicioUnoUno = new double[10];
 
         int ejercicioDos[] = {1, 2, 3, 4, 5};
 
-        double ejercicioTres[] = new double[10];
+        double ejercicioTres[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        double promedio = 0;
+        for ( double x: ejercicioTres) {
+            promedio += x;
+        }
+        promedio /= 10;
+
+        String[] ej4str = {"hola", "mundo", "adiós", "Angel", "uno", "dos", "millones", "cuatro"};
+
+        int ej4[] = {1, 3, 5, 7, 2, 4, 6, 9, 8, 0};
+        int guardar;
+        for ( int i=0; i<10; i++) {
+            for ( int j = 0; j<10; j++) {
+                if ( ej4[i] < ej4[j] ) {
+                    guardar = ej4[i];
+                    ej4[i] = ej4[j];
+                    ej4[j] = guardar;
+                }
+            }
+        }
+
+        for ( int i = 0; i<10; i++) {
+            System.out.print(ej4[i] + " ");
+        }
     }
 }
