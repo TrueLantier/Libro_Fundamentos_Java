27af8f3fb14f57945ff03314d0115fdd587d400d
Angel Eduardo Pedraza Ordoñez <leedgne@gmail.com>
2025-12-23 16:36:14 -0500

    Módulo 7.

--- src/Automotor.java
+++ src/Automotor.java
@@ -3,21 +3,21 @@ class Carro{
     private int tanquegas; // capacidad del tanque en litros
     private int kpl; // consumo de gasolina en km por litros
 
-    Carro( int p, int t, int k) {
+    Carro( int p, int f, int m) {
         pasajeros = p;
-        tanquegas = t;
-        kpl = k;
+        tanquegas = f;
+        kpl = m;
     }
     int rango(){
         return kpl*tanquegas;
     }
-    double gasnec( int kilómetros) {
+    double gasolinaNecesaria( int kilómetros) {
         return (double) kilómetros / kpl;
     }
 }
 
 class Camioneta extends Carro {
-    private int capacidadCarga;
+    private int capacidadCarga; // en kilos
 
     Camioneta(int p, int f, int m, int c) {
         super(p, f, m);
@@ -29,15 +29,8 @@ class Camioneta extends Carro {
 
 public class Automotor {
     public static void main(String[] args) {
-        Carro minivan = new Carro(7, 60, 6);
-        Carro deportivo = new Carro(2, 50, 4);
-        double litros;
-        int dist = 252; // Es en kilómetros.
+        Camioneta camioneta = new Camioneta(2, 800, 2, 20000);
 
-        litros = minivan.gasnec(dist);
-        System.out.println("Para recorrer " + dist + " km una minivan necesita " + litros + " de gasolina.");
 
-        litros = deportivo.gasnec(dist);
-        System.out.println("Para recorrer " + dist + " km un deportivo necesita " + litros + " de gasolina.");
     }
 }
