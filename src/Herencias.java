2718d950e0587684e1182474ef92f24435105072
Angel Eduardo Pedraza Ordoñez <leedgne@gmail.com>
2025-12-28 06:53:30 -0500

    Módulo 7.

--- src/Herencias.java
+++ src/Herencias.java
@@ -1,30 +1,60 @@
 class FormaDosD {
     private double ancho;
     private double alto;
+    private String nombre;
+
     FormaDosD() {
         ancho = alto = 0;
     }
-    FormaDosD(double w, double h) {
+    FormaDosD(double w, double h, String n) {
         ancho = w;
         alto = h;
+        nombre = n;
     }
-    FormaDosD(double x) {
+    FormaDosD(double x, String n) {
         ancho = alto = x;
+        nombre = n;
     }
     FormaDosD(FormaDosD ob) {
         ancho = ob.ancho;
         alto = ob.alto;
+        nombre = ob.nombre;
     }
 
     double obtenerAncho() {return ancho;}
     double obtenerAlto() {return alto;}
     void establecerAncho(double w) {ancho = w;}
     void establecerAlto(double h) {alto = h;}
+    String getNombre() { return nombre; }
+
     void mostrarDim() {
         System.out.println("El ancho y alto son: " + ancho + " " + alto);
     }
+    double area() {
+        System.out.println("area() debe sobreescribirse");
+        return 0.0;
+    }
 }
+class Rectang extends FormaDosD {
+    Rectang() {
+        super();
+    }
+    Rectang(double w, double h) {
+        super(w, h, "un rectángulo");
+    }
+    Rectang(double x) {
+        super(x, "un rectángulo");
+    }
+    Rectang(Rectang ob) {
+        super(ob);
+    }
 
+    boolean esCuadrado() {
+        return obtenerAncho() == obtenerAlto();
+    }
+
+    double area() { return obtenerAncho() * obtenerAlto(); }
+}
 class TriangDos extends FormaDosD {
     private String estilo;
 
@@ -33,11 +63,11 @@ class TriangDos extends FormaDosD {
         estilo = null;
     }
     TriangDos(String s, double w, double h) {
-        super(w,h);
+        super(w,h, "un triángulo");
         estilo = s;
     }
     TriangDos(double x) {
-        super(x);
+        super(x, "un triángulo");
         estilo = "isósceles";
     }
     TriangDos(TriangDos ob) {
@@ -52,7 +82,6 @@ class TriangDos extends FormaDosD {
         System.out.println("El triángulo es: " + estilo);
     }
 }
-
 class ColorTriang extends TriangDos{
     private String color;
     ColorTriang(String c, String s, double w, double h) {
@@ -77,28 +106,50 @@ public class Herencias {
 
 // Interesante
 class A {
-    A() {
-        System.out.println("Construyendo A.");
+    int i, j;
+    A(int a, int b) {
+        i = a;
+        j = b;
     }
-    static {
-        System.out.println("Esto va primero en A.");
+
+    void mostrar() {
+        System.out.println(i + " " + j);
     }
+//    A() {
+//        System.out.println("Construyendo A.");
+//    }
+//    static {
+//        System.out.println("Esto va primero en A.");
+//    }
 }
 class B extends A {
-    B() {
-        System.out.println("Construyendo B.");
+    int k;
+    B(int a, int b, int c) {
+        super(a,b);
+        k = c;
     }
-    static {
-        System.out.println("Esto va primero en B.");
+
+    void mostrar(String msj) {
+        //super.mostrar();
+        System.out.println(msj + k);
     }
+//    B() {
+//        System.out.println("Construyendo B.");
+//    }
+//    static {
+//        System.out.println("Esto va primero en B.");
+//    }
 }
 class C extends B {
-    C() {
-        System.out.println("Construyendo C.");
+    C(int a, int b, int c, int d) {
+        super(a, b, c);
     }
-    static {
-        System.out.println("Esto va primero en C.");
-    }
+//    C() {
+//        System.out.println("Construyendo C.");
+//    }
+//    static {
+//        System.out.println("Esto va primero en C.");
+//    }
 }
 
 class X {
--- src/ParaEjemplos.java
+++ src/ParaEjemplos.java
@@ -1,5 +1,33 @@
 public class ParaEjemplos {
     public static void main(String[] args) {
+        Sup ob = new Sup();
+        Sup1 ob1 = new Sup1();
+        Sup2 ob2 = new Sup2();
 
+        Sup opref;
+        opref = ob;
+        opref.quien();
+
+        opref = ob1;
+        opref.quien();
+
+        opref = ob2;
+        opref.quien();
+    }
+}
+
+class Sup{
+    void quien() {
+        System.out.println("quien() en Sup");
+    }
+}
+class Sup1 extends Sup{
+    void quien() {
+        System.out.println("quien() en Sup1");
+    }
+}
+class Sup2 extends Sup{
+    void quien() {
+        System.out.println("quien() en Sup2");
     }
 }
