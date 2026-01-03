79b301dd19c21353d2f0f64ee2c6ae8466a9fa68
Angel Eduardo Pedraza Ordoñez <leedgne@gmail.com>
2026-01-03 16:07:24 -0500

    Módulo 8. Paquetes e interfaces.

--- src/Proyecto1/Serie.java
+++ src/Proyecto1/Serie.java
@@ -9,14 +9,17 @@ public interface Serie {
 class PorDos implements Serie {
     int inicio;
     int val;
+    int ant;
 
     PorDos() {
         inicio = 0;
         val = 0;
+        ant = -2;
     }
 
     @Override
     public int getNext(){
+        ant = val;
         val += 2;
         return val;
     }
@@ -25,13 +28,52 @@ class PorDos implements Serie {
     public void reset() {
         inicio = 0;
         val = 0;
+        ant = -2;
     }
 
     @Override
     public void setStart(int x) {
         inicio = x;
         val = x;
+        ant = x - 2;
     }
+
+    int getPrior(){ return ant; }
+}
+
+class PorTres implements Serie {
+    int inicio;
+    int val;
+    int ant;
+
+    PorTres() {
+        inicio = 0;
+        val = 0;
+        ant = -3;
+    }
+
+    @Override
+    public int getNext(){
+        ant = val;
+        val += 3;
+        return val;
+    }
+
+    @Override
+    public void reset() {
+        inicio = 0;
+        val = 0;
+        ant = -3;
+    }
+
+    @Override
+    public void setStart(int x) {
+        inicio = x;
+        val = x;
+        ant = x - 3;
+    }
+
+    int getPrior(){ return ant; }
 }
 
 class SerieDemo {
