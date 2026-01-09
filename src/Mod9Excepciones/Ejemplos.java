e804b54ebeac5841fdb1f0eac884e16877c88e96
Angel Eduardo Pedraza Ordoñez <leedgne@gmail.com>
2026-01-09 14:16:11 -0500

    Módulo 9. Manejo de excepciones.

--- src/Mod9Excepciones/Ejemplos.java
+++ src/Mod9Excepciones/Ejemplos.java
@@ -1,4 +1,24 @@
 package Mod9Excepciones;
+import java.lang.ArrayIndexOutOfBoundsException;
 
 public class Ejemplos {
+    public static void main(String[] args) {
+        int[] nums = new int[4];
+
+        try {
+            System.out.println("Antes de que se genere la excepción.");
+            nums[7] = 10;
+            System.out.println("Esto no se muestra.");
+        } catch (Exception e) {
+            System.out.println("Índice fuera de límite.");
+        }
+        System.out.println("Tras el catch");
+    }
+}
+
+class ExceptionPrueba {
+    static void genExc() {
+        int[] nums = new int[4];
+        System.out.println("Antes de la excepción.");
+    }
 }
