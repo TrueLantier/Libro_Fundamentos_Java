a4d000a09fccab09d23fd22995bb53208d3451e7
Angel Eduardo Pedraza Ordoñez <leedgne@gmail.com>
2026-01-11 14:45:05 -0500

    Módulo 9. Manejo de excepciones.

--- src/Mod9Excepciones/Ejemplos.java
+++ src/Mod9Excepciones/Ejemplos.java
@@ -30,7 +30,6 @@ public class Ejemplos {
                 System.out.println("Índice fuera de límites.");
             }
         }
-
     }
 }
 
--- src/Mod9Excepciones/EjemplosDos.java
+++ src/Mod9Excepciones/EjemplosDos.java
@@ -2,17 +2,27 @@ package Mod9Excepciones;
 
 public class EjemplosDos {
     public static void main(String[] args) {
-        int[] numer = {4, 8, 16, 32, 64, 128};
+        int[] numer = {4, 8, 16, 32, 64, 128, 256, 512};
         int[] denom = {2, 0, 4, 4, 0, 8};
 
-        for (int i = 0; i < numer.length; i++) {
-            try {
-                System.out.println(numer[i] + " / " + denom[i] + " es " + numer[i] / denom[i]);
-            } catch (ArithmeticException exc) {
-                System.out.println("No se puede dividir entre 0.");
-            } catch (ArrayIndexOutOfBoundsException exc) {
-                System.out.println("Índice fuera de límites.");
+        try {
+            for (int i = 0; i < numer.length; i++) {
+                try {
+                    System.out.println(numer[i] + " / " + denom[i] + " es " + numer[i] / denom[i]);
+                } catch (ArithmeticException exc) {
+                    System.out.println("No se puede dividir entre 0.");
+                }
             }
+        } catch (ArrayIndexOutOfBoundsException exc) {
+            System.out.println("No hay elemento coincidente.");
+            System.out.println("Error fatal. Programa terminado.");
+        }
+
+        try {
+            System.out.println("Antes del lanzamiento.");
+            throw new ArithmeticException();
+        } catch (ArithmeticException e) {
+            System.out.println("Excepción capturada.");
         }
     }
 }
