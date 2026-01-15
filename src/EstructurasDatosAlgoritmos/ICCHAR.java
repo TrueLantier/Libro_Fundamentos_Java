8f75cc932ae37be1a0cf3ed1c1a64b9d405db24d
Angel Eduardo Pedraza Ordoñez <leedgne@gmail.com>
2026-01-15 17:51:57 -0500

    Empezar Módulo 10: Uso de I/O

--- src/EstructurasDatosAlgoritmos/ICCHAR.java
+++ src/EstructurasDatosAlgoritmos/ICCHAR.java
@@ -4,3 +4,13 @@ public interface ICCHAR {
     char get() throws ColaVacíaExcepción;
     void reset();
 }
+
+/*
+Interesante. ColaLlenaExcepción y ColaVacíaExcepción tiene modificadores de acceso predeterminado en la
+clase pública Excepciones. Entonces yo establezco que los métodos put y get de la interfaz pública ICCHAR
+pueden lanzar ambas excepciones respectivamente.
+Al la interfaz ser pública puede ser usada por clases de otros paquetes, y ellas podrán implementar sus
+métodos. Sin embargo, como las excepciones son predeterminadas no podrán ser arrojas en otros paquetes,
+para ese necesitarían tener su propio archivo y ser públicas.
+Esto se traduce en: Problemas de visibilidad y encapsulamiento.
+ */
