d1cde33d6d5d1b35fd29a5950c761fe1650a5998
Angel Eduardo Pedraza Ordoñez <leedgne@gmail.com>
2026-01-14 20:41:53 -0500

    Módulo 9. Manejo de excepciones. Hacer las comprobaciones.

---
+++ src/Comprobaciones/ComprobaciónMod9.java
@@ -0,0 +1,17 @@
+package Comprobaciones;
+
+public class ComprobaciónMod9 {
+    public static void main(String[] args) {
+        // 1: Throwable.
+        // 2: La cláusula try-catch se usa cuando un programa es propenso(por diversas razones) a generar
+        // un error. En el bloque try se escribe el código y, si ocurre una excepción entonces el
+        // programa cambia de ejecución al bloque catch, donde la excepción será capturada y habrá un código de
+        // respuesta.
+        // 3: No hay bloque try antes de la instrucción catch.
+        // 4: Si no se captura una excepción se obtiene la terminación anormal del programa.
+        // 5: La excepción de la superclase antecede a la de una subclase en los catch(su orden).
+        // 6: Sí, se pueden relanzar excepciones.
+        // 7: Falso, es el código ejecutado cuando se termina el bloque try-catch.
+        // 8:
+    }
+}
