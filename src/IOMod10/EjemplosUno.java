cf2cf91eb474d1906f5bf2dadfd711ddf89ee1cb
Angel Eduardo Pedraza Ordoñez <leedgne@gmail.com>
2026-01-31 18:02:55 -0500

    Empezar Módulo 10: Uso de I/O

--- src/IOMod10/EjemplosUno.java
+++ src/IOMod10/EjemplosUno.java
@@ -1,4 +1,22 @@
 package IOMod10;
+import EstructurasDatosAlgoritmos.*;
+import java.io.IOException;
 
 public class EjemplosUno {
+
+    public static void main(String[] args) throws IOException {
+        byte[] datos = new byte[5];
+        System.out.println("Escriba algunos caracteres.");
+        System.in.read(datos);
+        System.out.println("Usted escribió: ");
+        for (int i = 0; i < 5; i++) {
+            System.out.print((char) datos[i]);
+        }
+//        System.in.read();
+//        System.out.println();
+//        int b;
+//        b = 'X';
+//        System.out.write(b);
+//        System.out.write('\n');
+    }
 }
---
+++ src/IOMod10/MostrarArchivo.java
@@ -0,0 +1,8 @@
+package IOMod10;
+import java.io.*;
+
+public class MostrarArchivo {
+    public static void main(String[] args) {
+
+    }
+}
