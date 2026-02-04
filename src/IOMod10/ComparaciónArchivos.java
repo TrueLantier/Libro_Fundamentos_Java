7ab813d8d3a24678ad95ceb51ae9bb3f5ecb3264
Angel Eduardo Pedraza Ordoñez <leedgne@gmail.com>
2026-02-04 15:25:37 -0500

    Módulo 10: Uso de I/O

--- src/IOMod10/ComparaciónArchivos.java
+++ src/IOMod10/ComparaciónArchivos.java
@@ -1,9 +1,32 @@
 package IOMod10;
 import java.io.*;
 
-//Proyecto
+//Utilidad: Comparación de archivos.
 public class ComparaciónArchivos {
     public static void main(String[] args) {
+        int i = 0, j = 0;
 
+        if (args.length != 2) {
+            System.out.println("Uso: ComparaciónArchivos f1 f2");
+            return;
+        }
+
+        try (FileInputStream f1 = new FileInputStream(args[0]);
+             FileInputStream f2 = new FileInputStream(args[1])){
+
+            do {
+                i = f1.read();
+                j = f2.read();
+            }   while (i != -1 && j != -1);
+
+            if (i != j) {
+                System.out.println("Archivos diferentes.");
+                System.out.println(j);
+            }   else {
+                System.out.println("Archivos iguales");
+            }
+        }   catch (IOException exc) {
+            System.out.println("Error de E/S: " + exc);
+        }
     }
 }
