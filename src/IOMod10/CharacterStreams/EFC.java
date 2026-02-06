678ae79938f6a050fbb4345e22c9e637bc2518af
Angel Eduardo Pedraza Ordoñez <leedgne@gmail.com>
2026-02-06 09:00:30 -0500

    Módulo 10: Uso de I/O

---
+++ src/IOMod10/CharacterStreams/EFC.java
@@ -0,0 +1,7 @@
+package IOMod10.CharacterStreams;
+import java.io.*;
+//Entrada de la Consola usando Flujos de Caracteres.
+
+public class EFC {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in, System.console().charset()));
+}
