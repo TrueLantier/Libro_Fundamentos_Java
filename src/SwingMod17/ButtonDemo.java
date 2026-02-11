2390221e4b016798990ead00839567828c9cdf32
Angel Eduardo Pedraza Ordoñez <leedgne@gmail.com>
2026-02-10 20:32:55 -0500

    Módulo 10: Uso de I/O. Módulo 17: Swing.

--- src/SwingMod17/ButtonDemo.java
+++ src/SwingMod17/ButtonDemo.java
@@ -8,12 +8,17 @@ public class ButtonDemo implements ActionListener{
 
     ButtonDemo() {
         JFrame jFrame = new JFrame("Ejemplo Botón");
-        jFrame.setLayout(new FlowLayout());
+        jFrame.setLayout(new FlowLayout()); // Coloca los componentes en fila.
         jFrame.setSize(450, 450);
         jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         jLabel = new JLabel("Press a button.");
-        JLabel jLabel2 = new JLabel();
+
+        /*
+        ImageIcon imagen = new ImageIcon("src/SwingMod17/icono1.jpeg");
+        JLabel jLabel2 = new JLabel(imagen);
+        jFrame.add(jLabel2);
+         */
 
         JButton jbUp = new JButton("Up");
         JButton jbDown = new JButton("Down");
@@ -26,12 +31,14 @@ public class ButtonDemo implements ActionListener{
         jFrame.add(jbUp);
         jFrame.add(jbDown);
         jFrame.add(jLabel);
+
+        jFrame.setLocationRelativeTo(null); // Para que la GUI aparezca en el medio.
         jFrame.setVisible(true);
     }
 
     @Override
     public void actionPerformed(ActionEvent ae) {
-        if (ae.getActionCommand().equals("Up")) {
+        if (ae.getActionCommand().equals("Up")) { // texto del botón
             jLabel.setText("You pressed Up.");
         }   else {
             jLabel.setText("You pressed Down.");
---
+++ src/SwingMod17/TFDemo.java
@@ -0,0 +1,61 @@
+package SwingMod17;
+import java.awt.event.*;
+import java.awt.*;
+import javax.swing.*;
+
+public class TFDemo implements ActionListener{
+    JTextField jtf;
+    JButton jbtnRev;
+    JLabel jlabPrompt, jlabContents;
+
+    TFDemo() {
+        JFrame jfr = new JFrame("Usar un campo de texto.");
+        jfr.setLayout(new FlowLayout());
+        jfr.setSize(240, 120);
+        jfr.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        jtf = new JTextField(10);
+        jtf.setActionCommand("myTF");
+
+        jbtnRev = new JButton("Reverse");
+
+        jtf.addActionListener(this);
+        jbtnRev.addActionListener(this);
+
+        jlabPrompt = new JLabel("Texto de entrada");
+        jlabContents = new JLabel("");
+
+        jfr.add(jlabPrompt);
+        jfr.add(jtf);
+        jfr.add(jbtnRev);
+        jfr.add(jlabContents);
+        jfr.setLocationRelativeTo(null);
+        jfr.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent ae) {
+        if (ae.getActionCommand().equals("Reverse")) {
+            // El ActionCommand por defecto del JTextField es lo que hay en el campo de texto.
+            String orgStr = jtf.getText();
+            String resStr = "";
+
+            for (int i = orgStr.length()-1; i >= 0; i--) {
+                resStr += orgStr.charAt(i);
+            }
+            jtf.setText(resStr);
+        }   else {
+            jlabContents.setText("Has presionado ENTER. El text es: " + jtf.getText());
+        }
+        ae.getSource();
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new TFDemo();
+            }
+        });
+    }
+}
