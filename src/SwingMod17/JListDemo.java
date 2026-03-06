6c3368482e94e338c6ec22c29b9d775b5135612a
Angel Eduardo Pedraza Ordoñez <leedgne@gmail.com>
2026-03-06 07:03:45 -0500

    Practicando Swing.

--- src/SwingMod17/JListDemo.java
+++ src/SwingMod17/JListDemo.java
@@ -1,11 +1,10 @@
 package SwingMod17;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.*;
 import javax.swing.event.*;
 import javax.swing.*;
 
-public class JListDemo implements ListSelectionListener, ActionListener{
+public class JListDemo implements ListSelectionListener, ActionListener {
     JList<String> jList;
     JLabel jLabel, jLabel2;
     JButton jbtn;
---
+++ src/SwingMod17/MenuDemo.java
@@ -0,0 +1,9 @@
+package SwingMod17;
+import java.awt.event.*;
+import java.awt.*;
+import javax.swing.*;
+import com.formdev.flatlaf.*;
+
+public class MenuDemo {
+
+}
